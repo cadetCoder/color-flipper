eac8e3c7c434ab7146316aba60754e4032120fd7
Darrel Toledo <darreltoledo@icloud.com>
2021-07-02 22:14:11 +0100

    event listener when clicked

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const colors =["green", "red", "rgba(133, 122, 200)", "#f15025"];
 const btn = document.getElementById("btn");
 const colour = document.querySelector(".color");
+
+btn.addEventListener ('click', function () {
+  const randonNumber = 2;
+})
