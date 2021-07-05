80eb88ae7b280ce687a0444f4be80f157cbbb0a6
Darrel Toledo <darreltoledo@icloud.com>
2021-07-05 20:57:09 +0100

    bgcolor function

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ const body = document.body;
 function randomColor(){
   return Math.floor(Math.random()*255);
 }
+
+color.addEventListener('click',bgColor)
+
+function bgColor() {
+  const color1 = randomColor();
+  const color2 = randomColor();
+  const color3 = randomColor();
+
+  const rgbString = `rgb(${color1},${color2},${color3})`
+
+  body.style.background = rgbString;
+  textColor.innerText = rgbString;
+}
